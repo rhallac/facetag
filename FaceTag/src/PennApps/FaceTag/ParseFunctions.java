71f0e70118969c4dfe1154a077178eab3daaeb91
Rebecca Hallac <rhallac@seas.upenn.edu>
2014-09-13 19:00:26 -0400

    I WROTE CODEgit add --allgit add --all

--- FaceTag/src/PennApps/FaceTag/ParseFunctions.java
+++ FaceTag/src/PennApps/FaceTag/ParseFunctions.java
@@ -142,6 +142,7 @@ public class ParseFunctions {
 	}
 
 	
+	
 	public static List getUsersForGame(String gameName) {
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Game");
 		query.whereEqualTo("name", gameName);
@@ -175,5 +176,39 @@ public class ParseFunctions {
 	public void getImagesForGame(String gameId) {
 		
 	}
+	
+	
+
+	//takes game name and user name and increment score --> CHECK
+	public static void checkUserGame(String gameName, String userName) {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Game");
+		query.whereEqualTo("name", gameName);
+		query.whereEqualTo("username", userName);
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+		  public void done(List<ParseObject> objects, ParseException e) {
+		    if (e == null) {
+		        // The query was successful.
+		    	if(objects.size() != 0) {
+		    		ParseObject gameScore = new ParseObject("score");
+		    		gameScore.increment("score");
+		    	}
+		    
+		    } else {
+		        // Something went wrong.
+		    	System.out.println("incrementing score" + e);
+		    	
+		    }
+			};
+		});
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
