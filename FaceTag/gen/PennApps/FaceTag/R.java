dc4d9fafab41152511e87822abc5dfb51a6d5d46
Rebecca Hallac <rhallac@seas.upenn.edu>
2014-09-13 00:18:48 -0400

    copy and paste from tutorial so many issues

--- FaceTag/gen/PennApps/FaceTag/R.java
+++ FaceTag/gen/PennApps/FaceTag/R.java
@@ -21,13 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_login=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int login_button_focused=0x7f020002;
+        public static final int login_button_normal=0x7f020003;
+        public static final int login_button_pressed=0x7f020004;
+        public static final int table_round_shape=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080002;
+        public static final int loginButton=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int login_page=0x7f030001;
+        public static final int pleasework=0x7f030002;
+        public static final int profile_screen=0x7f030003;
+        public static final int userdetails=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- FaceTag/src/PennApps/FaceTag/MainActivity.java
+++ FaceTag/src/PennApps/FaceTag/MainActivity.java
@@ -1,34 +1,94 @@
 package PennApps.FaceTag;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button loginButton;
+	private Dialog progressDialog;
+	String APPID = "356060914569407";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
      
         Parse.initialize(this, "L6co5nhzzT7A9UvGcwAFbWV7WlSuw270GukGB0pq", "rXXwBQR639JmTTBm3cHbivV7NzKLRI09fmlYXWmV");     
         setContentView(R.layout.activity_main);
-        ParseFacebookUtils.initialize("YOUR FACEBOOK APP ID");
+        ParseFacebookUtils.initialize("356060914569407");
+        String appsecret = "1C85B9DCF3C269DEC37A8E1454753ED8";
         
        // ParseFunctions.addGame("new game", "123");
        // ParseFunctions.addUser("newuser", "123@123.com");
         
         //new ParseAddUser().execute(this, "bob", "bob@bob", 0);
+        
+		loginButton = (Button) findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLoginButtonClicked();
+			}
+		});
+
+		// Check if there is a currently logged in user
+		// and they are linked to a Facebook account.
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			showUserDetailsActivity();
+		}
+        
     }
+    
+    private void onLoginButtonClicked() {
+        MainActivity.this.progressDialog = ProgressDialog.show(
+                MainActivity.this, "", "Logging in...", true);
+        List<String> permissions = Arrays.asList("public_profile", "user_friends", "user_about_me",
+                "user_relationships", "user_birthday", "user_location");
+        ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+                MainActivity.this.progressDialog.dismiss();
+                if (user == null) {
+                    Log.d(IntegratingFacebookTutorialApplication.TAG,
+                            "Uh oh. The user cancelled the Facebook login.");
+                } else if (user.isNew()) {
+                    Log.d(IntegratingFacebookTutorialApplication.TAG,
+                            "User signed up and logged in through Facebook!");
+                    showUserDetailsActivity();
+                } else {
+                    Log.d(IntegratingFacebookTutorialApplication.TAG,
+                            "User logged in through Facebook!");
+                    showUserDetailsActivity();
+                }
+            }
+        });
+    }
+    
+    
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
       super.onActivityResult(requestCode, resultCode, data);
       ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
     }
+    
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
